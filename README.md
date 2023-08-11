e3605ad98efafc530a14c5f55c7d93756ad0a450
dopalescent <skudlgre000@gmail.com>
2023-08-10 23:08:01 -0400

    added notation to all js sheets, added demo video link to readme, will finish readme next

---
+++ README.md
@@ -0,0 +1,1 @@
+https://drive.google.com/file/d/1cOWnd62DQjdbBNquqn8kCCjKCI0J3vp9/view
--- index.js
+++ index.js
@@ -1,9 +1,11 @@
+// Requirements
 const fs = require('fs');
 const inquirer = require('inquirer');
+// Imported questions array and shape classes
 const questions = require('./lib/questions.js');
 const { Circle, Square, Triangle } = require('./lib/shapes.js');
 
-
+// Reads shape selection from answers, creates and returns new object from respective class
 const setShape = (shapeSubmit, shapeColor) => {
   if(shapeSubmit === 'circle') {
     const logoShape = new Circle(shapeColor);
@@ -17,11 +19,11 @@ const setShape = (shapeSubmit, shapeColor) => {
   };
 };
 
-
+// Plugs answers into svg template, then writes file from modified template
 function writeToFile(textSubmit, textColor, shapeSubmit, shapeColor) {
-
+  // Calls function for reading shape class selection
   const logoShape = setShape(shapeSubmit, shapeColor);
-
+  // Template for svg file, calls render function of child class of shape class
   const svgTemplate = `
 <svg version="1.1"
 width="300" height="200"
@@ -30,21 +32,23 @@ ${logoShape.render()}
 <text x="150" y="125" font-size="60" text-anchor="middle" fill="${textColor}">${textSubmit}</text>
 </svg>
 `;
-
+  // Creates and writes new svg file from template if there are no errors
   fs.writeFile('logo.svg', svgTemplate, (error) => {
     error ? console.error(error) : console.log('\n Generated logo.svg');
   });
 };
 
-
+// Main procedure function that initiates prompts, then uses the answers to call writeFile() 
 function init() {
   inquirer
+    // Initiates prompts with questions array
     .prompt(questions)
     .then((answers) => {
+      // Destructured answers array for use by writeToFile()
       const { textSubmit, textColor, shapeSubmit, shapeColor } = answers;
       writeToFile(textSubmit, textColor, shapeSubmit, shapeColor);
     });
 };
 
-
+// Calls main procedure function when index.js is run with node
 init();
